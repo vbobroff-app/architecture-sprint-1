45cd05996e33e7cf327f6fa61f9e3ea74b92337e
Pavel Sukharev <psukharev@nntc.pro>
2024-11-07 21:21:09 +0700

    cards mfe

---
+++ frontend/microfrontend/cards/src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import Card from './Card';
+
+function Main({ cards, onEditProfile, onAddPlace, onEditAvatar, onCardClick, onCardLike, onCardDelete }) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const imageStyle = { backgroundImage: `url(${currentUser.avatar})` };
+
+  return (
+    <main className="content">
+      <section className="places page__section">
+        <ul className="places__list">
+          {cards.map((card) => (
+            <Card
+              key={card._id}
+              card={card}
+              onCardClick={onCardClick}
+              onCardLike={onCardLike}
+              onCardDelete={onCardDelete}
+            />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
